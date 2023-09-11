8262fcaa9910f5bb63b1db990027acf3eb78db2a
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-09-11 18:10:20 +0300

    hero

--- fundme/src/Components/Filter.jsx
+++ fundme/src/Components/Filter.jsx
@@ -1,7 +1,7 @@
 function Filter() {
   return (
     <div className="filter">
-      <div>
+      <div className="filter__left">
         <label>Filter by</label>
         <select>
           <option>Getting funded</option>
@@ -9,12 +9,14 @@ function Filter() {
           <option>Fully funded</option>
         </select>
       </div>
-      <label>Sort by</label>
-      <select>
-        <option>Name</option>
-        <option>Lowest sum raised</option>
-        <option>Higest sum raised</option>
-      </select>
+      <div className="filter__right">
+        <label>Sort by</label>
+        <select>
+          <option>Name</option>
+          <option>Lowest sum raised</option>
+          <option>Higest sum raised</option>
+        </select>
+      </div>
     </div>
   );
 }
--- fundme/src/Components/Fundraisers.jsx
+++ fundme/src/Components/Fundraisers.jsx
@@ -1,4 +1,7 @@
 import Filter from "./Filter";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 function Fundraisers() {
   return (
@@ -6,6 +9,53 @@ function Fundraisers() {
       <p className="fundraiser__header">Personal Campaigns Getting Funded</p>
       <div className="fundraiser__header--border"></div>
       <Filter></Filter>
+      <ul className="fundraiser__lists">
+        <li className="fundraiser__lists--list">
+          <div className="fundraiser__lists--list--left">
+            <div className="fundraiser__lists--list--left--name">
+              <p>Name</p>
+              <p>Surname</p>
+            </div>
+            <div>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod
+              eaque, vel officia molestias provident consequuntur nostrum
+              blanditiis magni. Cumque, ea.
+            </div>
+          </div>
+          <div className="fundraiser__lists--list--middle">
+            <p className="fundraiser__lists--list--middle--goal">
+              2000 &euro; goal
+            </p>
+            <p>1000 &euro; donated</p>
+            <div className="fundraiser__lists--list--middle--goal--border"></div>
+          </div>
+          <div className="fundraiser__lists--list--middle--donations">
+            <div className="fundraiser__lists--list--middle--donations--header">
+              <p>Thank you for your donations</p>
+              <FontAwesomeIcon icon={faHeart} />
+            </div>
+
+            <div className="fundraiser__lists--list--middle--donations--name">
+              <label>Name</label>
+              <input></input>
+            </div>
+            <div className="fundraiser__lists--list--middle--donations--name">
+              <label>Sum</label>
+              <input></input>
+            </div>
+            <button className="fundraiser__lists--list--middle--donations--button">
+              Donate
+            </button>
+          </div>
+          <div className="fundraiser__lists--list--right">
+            <ul>Last donations</ul>
+            <li>
+              <p>Rima</p>
+              <p>50 &euro;</p>
+            </li>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
