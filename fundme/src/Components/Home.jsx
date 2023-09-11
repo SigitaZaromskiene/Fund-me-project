7ca5efb17b65566a079526b4e9750b8de62a7dbd
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-09-11 14:25:04 +0300

    hero

--- fundme/src/Components/Home.jsx
+++ fundme/src/Components/Home.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+import { Global } from "../Contexts/Global";
+
 function Home() {
+  const { setRoute } = useContext(Global);
   return (
     <div className="hero-section">
       <div className="hero-section__img"></div>
+      <div className="hero-section__text">
+        <h2>Powerful, Personal Fundraising </h2>
+        <p>Fast, easy & trusted by millions - create your page for free!</p>
+        <button onClick={() => setRoute("fundraisers")}>
+          Start fundraising for free
+        </button>
+      </div>
     </div>
   );
 }
--- fundme/src/Components/Nav.jsx
+++ fundme/src/Components/Nav.jsx
@@ -1,5 +1,9 @@
 import { useContext } from "react";
 import { Global } from "../Contexts/Global";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 function Nav() {
   const { setRoute, route } = useContext(Global);
   return (
@@ -36,6 +40,7 @@ function Nav() {
           Sign in
         </button>
       </div>
+      <FontAwesomeIcon icon={faBars} className="icon" />
     </nav>
   );
 }
